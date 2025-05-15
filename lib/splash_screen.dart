b9461d97d4ec72303925b1d001adaff35efa2143
Vanessa3456 <vanessarosepaul7@gmail.com>
2025-05-15 14:57:20 +0300

    Removed comments in splash screen

--- lib/splash_screen.dart
+++ lib/splash_screen.dart
@@ -2,58 +2,9 @@
 
 import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:lottie/lottie.dart';
 import 'package:vanick/pages/landing_page.dart';
-import 'package:vanick/routes.dart';
 
-// class SplashScreen extends StatefulWidget {
-//   const SplashScreen({super.key});
-
-//   @override
-//   State<SplashScreen> createState() => _SplashScreenState();
-// }
-
-// class _SplashScreenState extends State<SplashScreen>
-//     with SingleTickerProviderStateMixin {
-//   @override
-//   void initState() {
-//     super.initState();
-//     SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersive);
-
-//     Future.delayed(const Duration(seconds: 5), () {
-//       Navigator.pushReplacementNamed(context, Routes.landing);
-//     });
-//   }
-
-//   @override
-//   void dispose() {
-//     SystemChrome.setEnabledSystemUIMode(
-//       SystemUiMode.manual,
-//       overlays: SystemUiOverlay.values,
-//     );
-//     super.dispose();
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       backgroundColor: Color(0xFFF5EEFA),
-//       // Peach background color
-
-//       body: Center(
-//         child: Text(
-//           'Vanick',
-//           style: TextStyle(
-//             fontSize: 36.0,
-//             fontWeight: FontWeight.bold,
-//             color: Colors.black87,
-//           ),
-//         ),
-//       ),
-//     );
-//   }
-// }
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
